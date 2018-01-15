0bf585cc309c998d7979cf2fddf7bc35dc306863
Mats Bos <mats.bos@immalle.be>
2018-01-15 09:50:08 +0100

    swap method

--- swap/Program.cs
+++ swap/Program.cs
@@ -10,6 +10,27 @@ namespace swap
     {
         static void Main(string[] args)
         {
+            int a = 6;
+            int b = 8;
+            int c = 20;
+            int d = 30;
+
+            swap(ref c, ref d);
+            swap(ref a, ref b);
+            swap(ref a, ref c);
+
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+        }
+        static void swap(ref int a, ref int b)
+        {
+            int aCopy;
+            aCopy = a;
+            a = b;
+            b = aCopy;
         }
     }
 }
